fbbd367de4126e669219d6f3fd256ee4588f9d59
najro <orjan@najro.com>
2022-05-09 10:24:20 +0200

    added functionality to setup webview and userdata folder in temp folder

--- FigurePreview/FigurePreviewForm.cs
+++ FigurePreview/FigurePreviewForm.cs
@@ -1,6 +1,7 @@
 ﻿using FigurePreview.Configuration;
 using FigurePreview.Factories;
 using FigurePreview.Models;
+using Microsoft.Web.WebView2.Core;
 using System;
 using System.Diagnostics;
 using System.IO;
@@ -30,7 +31,7 @@ namespace FigurePreview
             if (FigureConfiguration.Instance.FigurePreview.DynamicPathFile.Enabled)
             {
                 WatchDynamicPathFileModifications();
-            }
+            }            
         }
         
         public void VerifyConfigurationAndDisplayFigures(bool loadForm = false)
@@ -45,10 +46,10 @@ namespace FigurePreview
             ResetErrorMessage();
             EnableViewComponents();
             SetInitalSelectedPathFigureRootFolder();
-            DisplayFigures();
+            DisplayFigures(loadForm);
         }
           
-        private void DisplayFigures()
+        private void DisplayFigures(bool loadForm = false)
         {
             // verify that selectedPathFiguresRootFolder exists
             if (selectedPathFiguresRootFolder == null || string.IsNullOrWhiteSpace(selectedPathFiguresRootFolder) || !Directory.Exists(selectedPathFiguresRootFolder))
@@ -88,17 +89,20 @@ namespace FigurePreview
                 }
             }
 
-            // check if selected figure is already set, then try to pre selected this view
-            // this might occur when any changes of files in figure folder
-            if (selectedFigureItem != null)
+            if (!loadForm)
             {
-                listBoxDisplayItems.SelectedItem = selectedFigureItem;
-                ReloadHtmlView();
-            }
-            else
-            {
-                SetDefaultView();
-            }                        
+                // check if selected figure is already set, then try to pre selected this view
+                // this might occur when any changes of files in figure folder
+                if (selectedFigureItem != null)
+                {
+                    listBoxDisplayItems.SelectedItem = selectedFigureItem;
+                    ReloadHtmlView();
+                }
+                else
+                {
+                    SetDefaultView();
+                }
+            }            
         }
 
 
@@ -275,7 +279,18 @@ namespace FigurePreview
 
         private async Task InitializeAsync()
         {
-            await webView2FigureView.EnsureCoreWebView2Async(null);
+            if (FigureConfiguration.Instance.FigurePreview.TempPath.Enabled)
+            {
+                var userDataFolder = FigureConfiguration.Instance.FigurePreview.TempPath.Text;
+                var webView2Environment = await CoreWebView2Environment.CreateAsync(null, userDataFolder);
+                await webView2FigureView.EnsureCoreWebView2Async(webView2Environment);
+            }
+            else
+            {
+                await webView2FigureView.EnsureCoreWebView2Async(null);
+            }
+
+            SetDefaultView();
         }
         private void InitializeFactories(bool loadForm = false)
         {
